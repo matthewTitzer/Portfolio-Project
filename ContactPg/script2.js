254b6aa1b14275232ddbc831400a5bb042fef71c
matthewTitzer <matthew.titzer812@gmail.com>
2024-07-01 21:41:07 -0500

    Add files via upload

---
+++ ContactPg/script2.js
@@ -0,0 +1,8 @@
+let formVariable = document.querySelector('myform');
+
+
+formVariable.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // send data somewhere update div
+});
