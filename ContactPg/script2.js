db14c841a92038dc4929c46ecb856415266c0672
matthewTitzer <matthew.titzer812@gmail.com>
2024-07-02 22:47:45 -0500

    Delete ContactPg directory

--- ContactPg/script2.js
+++
@@ -1,9 +0,0 @@
-let formVariable = document.querySelector('myform');
-let slideIndex = 1;
-
-
-formVariable.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    // send data somewhere update div
-});
